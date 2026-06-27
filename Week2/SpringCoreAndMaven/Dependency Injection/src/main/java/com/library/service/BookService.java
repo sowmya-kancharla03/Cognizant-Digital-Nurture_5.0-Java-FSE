4ea39ce1e7fd8fa894476e9664a8572d63403ab4
Sowmya Kancharla <sowmya.kancharla03@gmail.com>
2026-06-27 22:27:18 +0530

    Added Week2 Spring Core and Maven exercises

---
+++ Week2/SpringCoreAndMaven/Dependency Injection/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter method for Dependency Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+        System.out.println("Book-Service Bean is Created implementing Dependency Injection");
+        bookRepository.displayRepository();
+    }
+}
